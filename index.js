c1fd14a9dcaf0d259643a397b6e4c62aa5b4e8aa
MaquinoBSC <martinare.ipn@gmail.com>
2022-12-21 21:44:05 -0600

    Se hace envio de primer SMS

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 require('dotenv').config();
 
-const accountSid = process.env.ACCOUNT_SID;
-const authToken = process.env.AUTH_TOKEN;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const myPhoneNumber = process.env.MY_PHONE_NUMBER;
+
+const client = require('twilio')(accountSid, authToken);
+
+client.messages.create({
+    to: myPhoneNumber,
+    from: '+13866663660',
+    body: 'Hola Maquino, desde twilio',
+}).then((message) => {
+    console.log('here');
+    console.log(message.sid);
+})
+.catch((error) => {
+    console.log(error);
+})
