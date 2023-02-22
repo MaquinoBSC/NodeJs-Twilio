47d192bf5cd8671569136186f67c04726ccd8d05
MaquinoBSC <martinare.ipn@gmail.com>
2023-02-22 14:00:00 +0100

    Se implenta ruta oara recibir mensaje desde el cliente

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Envio de mensajes desde el servidor
+
 require('dotenv').config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
@@ -7,13 +9,13 @@ const myPhoneNumber = process.env.MY_PHONE_NUMBER;
 const client = require('twilio')(accountSid, authToken);
 
 client.messages.create({
-    to: myPhoneNumber,
-    from: '+13866663660',
+        to: myPhoneNumber,
+        from: '+13866663660',
     body: 'Hola Maquino, desde twilio',
-}).then((message) => {
-    console.log('here');
+})
+.then((message) => {
     console.log(message.sid);
 })
 .catch((error) => {
     console.log(error);
-})
+});
